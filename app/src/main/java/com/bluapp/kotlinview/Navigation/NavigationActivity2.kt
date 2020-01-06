a2fc99f1b31283e2ae5f15a5fa0e25096597c4bb
adenuga558 <adenugaemma@gmail.com>
2020-01-06 20:04:33 +0100

    add change background on NavigationActivity2

--- app/src/main/java/com/bluapp/kotlinview/Navigation/NavigationActivity2.kt
+++ app/src/main/java/com/bluapp/kotlinview/Navigation/NavigationActivity2.kt
@@ -1,51 +1,29 @@
 package com.bluapp.kotlinview.Navigation
 
-import android.content.Intent
-import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.bluapp.kotlinview.R
 import com.google.android.material.navigation.NavigationView
 
-
-class NavigationActivity1 : AppCompatActivity() {
+class NavigationActivity2 : AppCompatActivity() {
     private var mDrawerLayout: DrawerLayout? = null
     private var mNavigationView: NavigationView? = null
     private var toolbar: Toolbar? = null
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_navigation1)
+        setContentView(R.layout.activity_navigation2)
         toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
         mDrawerLayout = findViewById(R.id.drawerLayout) as DrawerLayout
         mNavigationView = findViewById(R.id.navigationView) as NavigationView
-
+        mNavigationView!!.setBackgroundColor(Color.GRAY)
         val mDrawerToggle = ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name)
         mDrawerLayout!!.addDrawerListener(mDrawerToggle)
         mDrawerToggle.syncState()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.navigation_option, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        super.onOptionsItemSelected(item)
-        when (item.getItemId()) {
-            R.id.action_activitynavigation2 -> {
-                startActivity(Intent(this@NavigationActivity1, NavigationActivity2::class.java))
-                return true
-            }
-        }
-
-        return true
     }
 }
